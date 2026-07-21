e990d5bcdf4199d08963561e822e0d30c0f1824a
sanchalikolod <sanchalikolod@gmail.com>
2026-07-21 20:48:57 +0530

    Added swap program

---
+++ Basics/swap.py
@@ -0,0 +1,10 @@
+print("SWAP SYSTEM\n")
+a=float(input("Enter 1st number: "))
+b=float(input("Enter 2nd number: "))
+print("Before swapping: ")
+print("a = ",a,"\nb = ",b)
+temp=a
+a=b
+b=temp
+print("After swapping: ")
+print("a = ",a,"\nb = ",b)
