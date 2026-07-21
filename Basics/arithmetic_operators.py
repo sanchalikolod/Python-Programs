7be9fdb420f351e717a2fa91c6962537be5b2443
sanchalikolod <sanchalikolod@gmail.com>
2026-07-21 20:27:54 +0530

    Added arithmetic program

---
+++ Basics/arithmetic_operators.py
@@ -0,0 +1,25 @@
+print("ADDITION\n")
+a=float(input("Enter the first number: "))
+b=float(input("Enter the second number: "))
+c=a+b
+print("Addition of ",a," and ",b," is ",c)
+
+print("\n\nSUBTRACTION\n")
+a=float(input("Enter the first number: "))
+b=float(input("Enter the second number: "))
+c=a-b
+print("Subtraction of ",a," and ",b," is ",c)
+
+print("\n\nMUTLIPLICATION\n")
+a=float(input("Enter the first number: "))
+b=float(input("Enter the second number: "))
+c=a*b
+print("Multiplication of ",a," and ",b," is ",c)
+
+print("\n\nDIVISION\n")
+a=float(input("Enter the first number: "))
+b=float(input("Enter the second number: "))
+c=a/b
+d=a%b
+print("Quotient on division of ",a," and ",b," is ",c)
+print("Remainder on division of ",a," and ",b," is ",d)
