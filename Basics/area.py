037a15fa334c4efbbb6f7d645a05580cac336120
sanchalikolod <sanchalikolod@gmail.com>
2026-07-21 20:44:52 +0530

    Added area program

---
+++ Basics/area.py
@@ -0,0 +1,21 @@
+print("AREA OF CIRCLE\n")
+r=float(input("Enter radius of circle: "))
+area=3.14*r*r
+print("Area of circle is: ",area)
+
+print("\n\nAREA OF SQUARE\n")
+s=float(input("Enter side of square: "))
+area=s*s
+print("Area of square is: ",area)
+
+print("\n\nAREA OF TRIANGLE\n")
+b=float(input("Enter base of triangle: "))
+h=float(input("Enter height of triangle: "))
+area=0.5*b*h
+print("Area of triangle is: ",area)
+
+print("\n\nAREA OF RECTANGLE\n")
+l=float(input("Enter length of rectangle: "))
+b=float(input("Enter breadth of rectangle: "))
+area=l*b
+print("Area of rectangle is: ",area)
