103513f8c601e228bdcfb4d7784f7a0048033542
sanchalikolod <sanchalikolod@gmail.com>
2026-08-07 23:07:19 +0530

    Added second pattern program

---
+++ Problems in Python/pattern2.py
@@ -0,0 +1,10 @@
+#Pattern.
+
+n=int(input("Enter N: "))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end="")
+    for j in range(i-1,0,-1):
+        print(j,end="")
+    print()
+        
